2d9b1e581e5fd962b5f49ebe148d155ae3d039b1
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-21 22:09:18 +0530

    Added Listener for the Progress Bar!

--- app/src/main/java/com/bharathkalyans/memesapp/MainActivity.kt
+++ app/src/main/java/com/bharathkalyans/memesapp/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.bharathkalyans.memesapp
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadMeme() {
+
+        //Progress Bar is Set Visible
+        progressBar.visibility = View.VISIBLE
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
@@ -29,10 +36,34 @@ class MainActivity : AppCompatActivity() {
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
             { response ->
+
                 val url = response.getString("url").toString()
                 Glide
                     .with(this)
                     .load(url)
+                    .listener(object :  RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressBar.visibility = View.GONE
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            progressBar.visibility = View.GONE
+                            return false
+                        }
+
+                    })
                     .into(ivMemesImage);
             },
             {
@@ -48,7 +79,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun nextMeme(view: View) {
-
+        loadMeme()
     }
 
 }
